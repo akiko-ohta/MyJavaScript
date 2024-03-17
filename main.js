e402cf84357293d18f46f682790e7db13cac9ef1
Akiko Ohta <0249aki@gmail.com>
2024-03-17 22:02:23 +0900

    JavaScript入門　関数編6-9

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
 'use strict';
 
 {
-  function double (num) { //　仮引数
-    return num * 2;
-  }
-  
-  console.log(double(10)); // 実引数
-  console.log(double(4) * 3); // 実引数
+  function caluclateTotal(price, amount, rate = 1.1) {
+    // if (amount >= 100) {
+    //   return price * amount;
+    // } else {
+    // return price * amount * rate;
+    // }
+
+// 早期リターン
+    if (amount >= 100) {
+      return price * amount;
+    }
+    return price * amount * rate;
+    }
+
+  console.log(caluclateTotal (100, 100));
+  console.log(caluclateTotal (1000, 10));
 }
 
 {
-  function sum (a, b){
-    return a + b;
-   }
+  function double(num) {
+    return num * 2;
+  }
+
+  function triple(num) {
+    return num *3;
+  }
+
+  console.log(double(10));
+  // console.log(num);
+  console.log(triple(20))
+}
+
+{
   
-// console.log(sum(300, 700));
-// }
+  function showAd() {
+    console.log("---------");
+    console.log("SALE! 50% OFF!");
+    console.log("---------");
+  }
 
-// function sum (a, b){
-//  console.log (a + b);
-//  return undefined;
-// }
+  function showContent() {
+    console.log("BREAKING NEWS!");
+    console.log("Two baby pandas born at our Zoo!");
+  }
 
-// console.log(sum(300, 700));
-console.log(sum(300, 700)*3);
+  showAd();
+  showContent();
+  showAd();
 }
