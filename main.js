4d69d223849b18f371d35e6b84aedfc57148fbd9
Akiko Ohta <0249aki@gmail.com>
2024-04-26 12:24:23 +0900

    JavaScript入門　データ構造編17-19

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 'use strict';
 
 {
-  const scores = [70, 90, 80, 85];
+  // const names = ['Taro', 'Jiro', 'Saburo'];
+  // Taro|Jiro|Saburo
+  // console.log(names.join('|'));
+  // console.log(names.join(''));
+  // console.log(names.join(','));
 
-  scores.splice(2, 0, 77, 88);
-  // [70, 90, 77, 88, 80, 85]
+  // const names = 'Taro|Jiro|Saburo';
+  // console.log(names.split('|'));
 
-  const deleted = scores.splice(3,1);
-  // [70, 90, 77, 80, 85]
-  // [88]
+  const prices = [100, 150, 200];
 
-  scores.splice(2, 2, 30);
-  // [70, 90, 30, 85]
+  // const pricesOver150 = [];
+  // prices.forEach((price) => {
+  //   if (price >= 150) {
+  //     pricesOver150.push(price);
+  //   }
+  // });
 
-  console.log(scores);
-  console.log(deleted);
+  const pricesOver150 =prices.filter ((price) => {
+    return price >= 150;
+  });
+
+  console.log(pricesOver150);
+
+  // const pricesWithTax = [];
+  // prices.forEach((price) => {
+  //   pricesWithTax.push(price * 1.1);
+  // });
+
+  // const pricesWithTax = prices.map((price) => {
+  //   return price * 1.1
+  // });
+  // console.log(pricesWithTax)
 }
