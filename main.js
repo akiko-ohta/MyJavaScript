f3d851730cf5238cd9c2147675e3ff9525e78f1d
Akiko Ohta <0249aki@gmail.com>
2024-05-03 13:19:40 +0900

    JavaScript入門 DOM編1-4

--- main.js
+++ main.js
@@ -1,36 +1,13 @@
 'use strict';
 
 {
-//   const moreScores = {
-//     shiro: 77,
-//     goro: 88,
-//   };
+  document.querySelector('button').addEventListener('click', () => {
+    document.querySelector('p').classList.add('pink-bg', 'red-border');
+    document.querySelector('p').classList.remove('green-color');
+    // console.log('Clicked');
+    // document.querySelector('p').textContent = 'こんにちは';
+    // document.querySelector('p').textContent = document.querySelector('button').textContent;
+  });
 
-//   const scores = {
-//   taro: 80,
-//   jiro: 70,
-//   saburo: 90,
-//   ...moreScores,
-//  };
-// //  const {taro, jiro, saburo} = scores;
-// const {taro, ...others} = scores;
-
-//  console.log(taro);
-// //  console.log(jiro);
-// //  console.log(saburo);
-// console.log(others);
-
-  // let num = 10;
-  // const numBackup = num;
-  // num = 99;
-  // console.log(num); // 99
-  // console.log(numBackup) // 10
-
-  const nums = [10, 20, 30];
-  // const numBackup = nums;
-  // const numBackup = [10, 20, 30];
-  const numBackup = [...nums];
-  nums[0] = 99;
-  console.log(nums); // [99, 20, 30]
-  console.log(numBackup) // [10, 20, 30]
+  // console.log('Hello');
 }
