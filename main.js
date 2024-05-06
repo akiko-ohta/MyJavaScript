10bd5730ac1bc41be8cb4e13d502376e4602f118
Akiko Ohta <0249aki@gmail.com>
2024-05-06 14:47:32 +0900

    JavaScript入門 DOM編5

--- main.js
+++ main.js
@@ -2,11 +2,16 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    document.querySelector('p').classList.add('pink-bg', 'red-border');
-    document.querySelector('p').classList.remove('green-color');
+    // console.log(document.querySelector('p').classList.contains('pink-bg')); // false
+    // if (document.querySelector('p').classList.contains('pink-bg') === false) {
+    //   document.querySelector('p').classList.add('pink-bg');
+    // } else {
+    //   document.querySelector('p').classList.remove('pink-bg');
+    // }
     // console.log('Clicked');
     // document.querySelector('p').textContent = 'こんにちは';
     // document.querySelector('p').textContent = document.querySelector('button').textContent;
+    document.querySelector('p').classList.toggle('pink-bg');
   });
 
   // console.log('Hello');
