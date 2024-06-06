13b849cf6538bf1a6bed6fe39c8461f5470d021f
Akiko Ohta <0249aki@gmail.com>
2024-06-06 13:27:53 +0900

    JavaScript入門 数値操作編1

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 'use strict';
 
 {
- 
-  document.querySelector('button').addEventListener('click', (e) => {
-    // document.querySelector('p').style = 'font-size: 24px';
-    document.querySelector('p').style.fontSize = '24px';
-  });
+  const n = Number(prompt('Number?'));
+  console.log(n + 5);
 }
