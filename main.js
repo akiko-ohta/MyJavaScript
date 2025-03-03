48f3525e2fb434287d3b943ed5a2c1ef07aa0b44
Akiko Ohta <0249aki@gmail.com>
2025-03-03 22:45:28 +0900

    JavaScript入門 基礎文法編6

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 'use strict';
 
-/*
-JavaScriptにおける数値の表現
-@ohta
-*/
+console.log(10 + 3); // 13
+console.log(10 - 3); // 7
+console.log(10 * 3); // 30
+console.log(10 ** 3); // 1000
+console.log(10 / 3); // 3.3
+console.log(10 % 3); // 1
 
-// console.log(100);
-// console.log(-10);
-// console.log(2.5);
-
-//大きな数値、小さな数値の表現
-console.log(1.2e4); // 1.2かける10の4乗 12000
-console.log(1.2e-4); // 1.2かける10の-4乗＝ 0.00012
+console.log(10 + 2 * 3); // 16
+console.log((10 + 2) * 3); // 36
