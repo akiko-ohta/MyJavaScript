a6656eb9ce408ef4e9b5cf1d500b0b53467e2e8f
Akiko Ohta <0249aki@gmail.com>
2025-03-03 22:54:22 +0900

    JavaScript入門 基礎文法編7-10

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 'use strict';
 
-console.log(10 + 3); // 13
-console.log(10 - 3); // 7
-console.log(10 * 3); // 30
-console.log(10 ** 3); // 1000
-console.log(10 / 3); // 3.3
-console.log(10 % 3); // 1
+// console.log(150 * 120 * 1.1);
+// console.log(150 * 130 * 1.1);
+// console.log(150 * 140 * 1.1);
 
-console.log(10 + 2 * 3); // 16
-console.log((10 + 2) * 3); // 36
+// 変数
+let price;
+// 定数
+const rate = 1.1;
+
+price = 150;
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
+
+// price = 151;
+// price = price + 1;
+// price += 1;
+// price *=1.5;
+price++;
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
