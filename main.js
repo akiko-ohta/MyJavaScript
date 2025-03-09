ea8bcfc677b9d1fad763dd0bc9bef99ba97da5d4
Akiko Ohta <0249aki@gmail.com>
2025-03-09 20:57:47 +0900

    JavaScript入門 関数編10-12

--- main.js
+++ main.js
@@ -1,65 +1,52 @@
 'use strict';
 
-// JavaScript入門 関数編1-3
+// JavaScript入門 関数編9
 // {
-//   function double(num) { // 仮引数
-//     return num * 2; 
+//   function showAd() {
+//     console.log("---------");
+//     console.log("SALE! 50% OFF!");
+//     console.log("---------");
 //   }
-//   console.log(double(10)); // 実引数
-//   console.log(double(4) * 3);
-// }
-
-// JavaScript入門 関数編4-5
-// {
-//   function sum(a, b) {
-//     return a + b;
+//   function showContent() {
+//     console.log("BREAKING NEWS!");
+//     console.log("Two baby pandas born at our Zoo!");
 //   }
 
-//   console.log(sum(300, 700));
+//   showAd();
+//   showContent();
+//   showAd();
 // }
 
-// {
-//   function sum(a, b) {
-//     console.log(a + b);
-//     // return undefined;
-//   }
-
-  // console.log(sum(300, 700));
-  // console.log(sum(300, 700) * 3);
-// }
-
-// JavaScript入門 関数編6-7
-// {
-//   function calculateTotal(price, amount, rate = 1.1) {
-    // if (amount >= 100) {
-    //   return price * amount;
-    // } else {
-    //   return price * amount * rate;
-    // }
-    // 早期リターン
-  //   if (amount >= 100) {
-  //     return price * amount;
-  //   }
-  //     return price * amount * rate;
+// JavaScript入門 関数編10-12
+{
+  // 関数宣言
+  // function double(num) {
+  //   return num * 2;
   // }
 
-  // console.log(calculateTotal(100, 100));
-  // console.log(calculateTotal(1000, 10));
-  // console.log(calculateTotal(200, 10));
-  // console.log(calculateTotal(120, 10, 1.08));
-// }
+  // 関数式（無名関数）
+  //   const double = function(num) {
+  //   return num * 2;
+  // };
 
-// JavaScript入門 関数編8
-// {
-//   function double(num) {
-//     return num * 2;
-//   }
+  // アロー関数式
+  // const double = (num) => {
+  //   return num * 2;
+  // };
 
-// function triple(num) {
-//   return num * 3;
-// }
+  // const double = num => {
+  //   return num * 2;
+  // };
 
-//   console.log(double(10));
-//   // console.log(num);
-//   console.log(triple(20));
-// }
+  // const double = num => num * 2;
+
+  const calc = (num, func) => {
+    // return double()(20)
+    return func(num);
+  };
+
+  // console.log(calc(20, double));
+  console.log(calc(20, (num) => { 
+    return num * 2;
+  }));
+}
