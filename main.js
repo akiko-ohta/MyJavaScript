0aa4554259da3ae6c9d516372228e9551f918acc
Akiko Ohta <0249aki@gmail.com>
2025-03-13 22:44:24 +0900

    JavaScript入門 データ構造編#20-27

--- main.js
+++ main.js
@@ -2,57 +2,75 @@
 
 
 {
-  // JavaScript入門 データ構造編#15-16
-  // const scores = [70, 90, 80, 85];
+  // JavaScript入門 データ構造編#20
+  //   const scores = [70, 90, 80, 85];
+    // const first = scores[0];
+    // const second = scores[1];
+    // const third = scores[2];
+    // const fourth = scores[3];
+  //   const [first, second, third, fourth] = scores;
 
-  // scores.splice(2, 0, 77, 88);
-  // // [70, 90, 77, 88, 80, 85]
+  //   console.log(first);
+  //   console.log(second);
+  //   console.log(third);
+  //   console.log(fourth);
 
-  // const deleted = scores.splice(3, 1);
-  // // [70, 90, 77, 80, 85]
-  // // [88]
+  // JavaScript入門 データ構造編#21
+  // let start = 'Tokyo';
+  // let goal = 'Osaka';
 
-  // scores.splice(2, 2, 30);
-  // // [70, 90, 30, 85]
+  // start = goal; // start = Osaka
+  // goal = start; // goal = Osaka
 
-  // console.log(scores);
-  // console.log(deleted);
+  // let temp = '';
+  // temp = start;
+  // start = goal;
+  // goal = temp;
 
-  // JavaScript入門 データ構造編#17
-  // const names = ['Taro', 'Jiro', 'Saburo'];
+  // [goal, start] = [start, goal]
 
-  // Taro|Jiro|Saburo
-  // console.log(names.join('|'));
-  // console.log(names.join());
-  // console.log(names.join(''));
+  // console.log(start);
+  // console.log(goal);
 
-  // const names = 'Taro|Jiro|Saburo';
-  // console.log(names.split('|'));
+  // JavaScript入門 データ構造編#22-23
+  // const moreScores = [77, 88]
+  // const scores = [70, 90, 80, 85, ...moreScores];
+  // const [first, ...others] =scores;
 
-  // JavaScript入門 データ構造編#18-19
-  const prices = [100, 150, 200];
+  // console.log(first);
+  // console.log(others);
 
-  // const pricesOver150 = [];
-  // prices.forEach((price) => {
-  //   if (price >= 150) {
-  //     pricesOver150.push(price);
-  //   }
-  // });
+  // JavaScript入門 データ構造編#22-24
+  // const moreScores = {
+  //   shiro: 77,
+  //   goro: 88,
+  // };
+  // const scores = {
+  //   taro: 80,
+  //   jiro: 70,
+  //   saburo: 90,
+  //   ...moreScores,
+  // };
+  // // const {taro, jiro, saburo} = scores;
+  // const {taro, ...others} = scores;
 
-  const pricesOver150= prices.filter((price) => {
-    return price >= 150;
-  })
+  // console.log(taro);
+  // // console.log(jiro);
+  // // console.log(saburo);
+  // console.log(others);
 
-  console.log(pricesOver150);
+    // JavaScript入門 データ構造編#25-27
+    // let num = 10;
+    // const numBackup = num;
+    // num = 99;
+    // console.log(num); // 99
+    // console.log(numBackup); // 10
 
-  // const pricesWithTax = [];
-  // prices.forEach((price) => {
-  //   pricesWithTax.push(price * 1.1);
-  // });
-
-  // const pricesWithTax = prices.map((price) => {
-  //   return price * 1.1;
-  // });
-
-  // console.log(pricesWithTax);
+    const nums = [10, 20, 30];
+    // const numsBackup = nums;
+    // const numsBackup = [10, 20, 30];
+    const numsBackup = [...nums];
+    nums[0] = 99;
+    console.log(nums); // [99, 20, 30]
+    console.log(numsBackup); // [10, 20, 30]
 }
