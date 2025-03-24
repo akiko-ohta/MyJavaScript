ad71bc9b6f69871852f3db308e41db4e50bb26a2
Akiko Ohta <0249aki@gmail.com>
2025-03-24 23:52:28 +0900

    JavaScript入門 DOM編#19-22

--- main.js
+++ main.js
@@ -2,29 +2,16 @@
 
 
 {
-  // JavaScript入門 DOM編#15
-  // input → text, textarea, select
-  // change → radio, checkbox
-  // document.querySelector('input').addEventListener('input', () => {
-    // const pElement = document.querySelector('p');
-    // const inputElement = document.querySelector('input');
-    // pElement.textContent = inputElement.value;
-    // pElement.textContent = inputElement.value.length;
+  // JavaScript入門 DOM編#19
+  // document.querySelector('form').addEventListener('submit', (e) => {
+  //   e.preventDefault();
+  //   document.querySelector('p').textContent = document.querySelector('input').
+  //   value;
+  // });
 
-  // JavaScript入門 DOM編#16
-  //   document.querySelector('p').textContent = 'English only!';
-  // });
-  // document.querySelector('input').addEventListener('blur', () => {
-  //   document.querySelector('p').textContent = '';
-  // });
-  // document.querySelector('input').focus();
-  
-  // JavaScript入門 DOM編#17
-  // document.addEventListener('keydown', (e) => {
-  //   document.querySelector('p').textContent = e.key;
-  
-  // JavaScript入門 DOM編#18
-  document.addEventListener('mousemove', (e) => {
-    document.querySelector('p').textContent = `X: ${e.clientX} Y: ${e.clientY}`;
+  // JavaScript入門 DOM編#20-22
+  document.querySelector('button').addEventListener('click', () => {
+    // document.querySelector('p').style = 'font-size: 24px';
+    document.querySelector('p').style.fontSize = '24px';
   });
 }
