f6cf7ad77cad2a5bf4882b538ec8a5655755e8a9
Akiko Ohta <0249aki@gmail.com>
2025-03-25 23:04:48 +0900

    JavaScript入門 数値操作編#3-8

--- main.js
+++ main.js
@@ -2,12 +2,39 @@
 
 
 {
-  // JavaScript入門 数値操作編#1
-  // const n = Number(prompt('Number?'));
-  // console.log(n + 5);
+  // JavaScript入門 数値操作編#3
+  // const n = Number(prompt('Yen?'));
+  // const amount = (n / 138.91).toFixed(2);
+  // console.log(`USD: ${Number(amount).toLocaleString()}`);
 
-    // JavaScript入門 数値操作編#2
-    const n = Number(prompt('Minutes?'));
-    console.log(`Hours: ${Math.floor(n / 60)}`);
-    console.log(`Minutes: ${n % 60}`);
+  // JavaScript入門 数値操作編#4
+  // const n = Number(prompt('Count?'));
+  // for (let i = 0; i < n; i++) {
+  //   if ( i % 3 === 0) {
+  //     console.log(`---item ${i}---`);
+  //   } else {
+  //     console.log(`===item ${i}===`);
+  //   }
+  // }
+  // JavaScript入門 数値操作編#5-6
+  // function getRandomInteger(max) {
+  //   return Math.floor(Math.random() * (max+1));
+  // }
+  // for (let i = 0; i < 10; i++) {
+  //   const n = getRandomInteger(5) + 1;
+  //   console.log(`${i}: ${n}`);
+  // }
+
+  // JavaScript入門 数値操作編#7-8
+  const scores = [70, 90, 80, 75];
+  // let max = 0;
+
+  // scores.forEach((score) => {
+  //   if (score > max) {
+  //     max = score;
+  //   }
+  // });
+  // console.log(max);
+  console.log(Math.max(...scores)); // 90
+  console.log(Math.min(...scores)); // 70
 }
