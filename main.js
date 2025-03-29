30e33ce71308853063564f371a94bf8bd98be341
Akiko Ohta <0249aki@gmail.com>
2025-03-29 21:52:06 +0900

    JavaScript入門 タイマー編#1-10

--- main.js
+++ main.js
@@ -2,64 +2,59 @@
 
 
 {
-  // JavaScript入門 日時操作編#1-2
-  // const d = new Date();
-  // console.log(d);
-  // console.log(d.toLocaleString());
-  // console.log(d.getFullYear());
-  // console.log(d.getMonth()); // 0, 1, 2, ...
-  // console.log(d.getDate());
-  // console.log(d.getHours());
-  // console.log(d.getMinutes());
-  // console.log(d.getSeconds());
-  // console.log(d.getMilliseconds());
-  // console.log(d.getDay()); // 0, 1, 2, ...
+  // JavaScriptタイマー編#1-2
+//   console.log(new Date());
+//   setInterval(() => {
+//   console.log(new Date());
+//  }, 1000);
 
-  // JavaScript入門 日時操作編#3
-  // 2000 4 11
-  // const d = new Date(2000, 3, 11);
-  //2000 2 ??
-  // const d = new Date(2000, 2, 0);
-  // console.log(d.toLocaleString());
+  // JavaScriptタイマー編#3-5
+  // let count = 3;
 
-  // JavaScript入門 日時操作編#4-6
-  // 2000 4 11
-  // const d = new Date(2000, 3, 11);
-  // const dBacnup = d;
-  // const dBacnup = new Date(d.getFullYear(), d.getMonth(), d.getDate());
-  // const dBacnup = new Date(d.getTime());
-  // d.setDate(23);
-  // d.setDate(d.getDate() + 100);
+  // const intervalID = setInterval(() => {
+  //   console.log(count);
+  //   count--;
+  //   if (count < 0) {
+  //     clearInterval(intervalID);
+  //   }
+  // }, 1000);
 
-  // console.log(d.toLocaleString());
-  // console.log(dBacnup.toLocaleString());
+  // console.log(`ID: ${intervalID}`);
 
-  // console.log(d);
-  // UTC
-  // 1970/01/01 00:00:00
-  // Unix Timestamp
-  // console.log(d.getTime());
+  // JavaScriptタイマー編#3-5
+  // setTimeout(() => {
+    // console.log('50% OFF!')
+  //   window.location.href = 'http://256times.com';
+  // }, 3000);
 
-  // const d2= new Date(d.getTime() + 1000);
-  // console.log(d2);
+  // console.log('Page loaded')
 
-  // JavaScript入門 日時操作編#7
-  // 2023 6 10
-  // 
-  // const diff = new Date().getTime() - new Date(2023, 5, 10).getTime();
-  // const diff = Date.now().getTime() - new Date(2023, 5, 10).getTime();
-  // const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-  
-  // console.log(diff);
-  // console.log(days);
+  // JavaScriptタイマー編#6-10
+  // setTimeout(() => {
+  //   console.log(new Date());
+  //   setTimeout(() => {
+  //     console.log(new Date());
+  //     setTimeout(() => {
+  //       console.log(new Date());
+  //     }, 1000);
+  //   }, 1000);
+  // }, 1000);
 
-  // JavaScript入門 日時操作編#8
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const d = new Date(2001, 0, 1);
-  // January 01, Monday
-  const month = months[d.getMonth()];
-  const date = String(d.getDate()).padStart(2, '0');
-  const day = days[d.getDay()];
-  console.log(`${month} ${date}, ${day}`);
+  const pElement = document.querySelector('p');
+  const buttonElement = document.querySelector('button');
+  let timeoutId;
+
+  // 再帰関数
+  function showClock() {
+    timeoutId = setTimeout(() => {
+      pElement.textContent = new Date();
+      showClock();
+    }, 1000);
+  }
+
+  buttonElement.addEventListener('click', () => {
+    clearTimeout(timeoutId);
+  });
+
+  showClock();
 }
